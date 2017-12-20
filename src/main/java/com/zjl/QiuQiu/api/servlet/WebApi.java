1c2371ab433f9916760427316c62f72384e29339
zhengjialeiwb <295558096@qq.com>
2017-12-20 20:02:56 +0800

    首页

---
+++ src/main/java/com/zjl/QiuQiu/api/servlet/WebApi.java
@@ -0,0 +1,7 @@
+package com.zjl.QiuQiu.api.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class WebApi extends HttpServlet {
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-<html>
+	<html>
 <script type="text/javascript">
 	window.location.href="http://172.18.72.41:8092/#/"; 
 </script>
